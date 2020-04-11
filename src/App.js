96de9ed8498bd0bd4b7c02e2118eb4c58bea867e
Sonavane, Dhiraj <Dhiraj.Sonavane@tabcorp.com.au>
2020-04-11 16:57:38 +1000

    modified readme file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+import NavBar from "./shared/NavBar";
+import Footer from "./shared/Footer";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: [],
+    };
+  }
+
+  addItem(todoValue) {
+    if (todoValue !== "") {
+      const newItem = {
+        id: Date.now(),
+        value: todoValue,
+        isDone: false,
+      };
+      const list = [...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list,
+        newItem: "",
+      });
+    }
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedlist = list.filter((item) => item.id !== id);
+    this.setState({ list: updatedlist });
+  }
+
+  updateInput(input) {
+    this.setState({ newItem: input });
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+
+        <Container style={{ display: "flex", justifyContent: "center" }}>
+          <Card style={{ width: 500, margin: 10 }}>
+            <CardContent>
+              <TextField
+                id="outlined-basic"
+                label="Enter a Todo"
+                variant="outlined"
+                value={this.state.newItem}
+                onChange={(e) => this.updateInput(e.target.value)}
+                style={{ marginRight: 10 }}
+                required
+              />
+
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => this.addItem(this.state.newItem)}
+                disabled={!this.state.newItem.length}
+                style={{ marginTop: 10 }}
+              >
+                Add Todo
+              </Button>
+
+              <div className="list">
+                <ul>
+                  {this.state.list.map((item) => {
+                    return (
+                      <li key={item.id}>
+                        <Checkbox
+                          name="idDone"
+                          checked={item.isDone}
+                          onChange={() => {}}
+                          inputProps={{ "aria-label": "primary checkbox" }}
+                        />
+
+                        {item.value}
+
+                        <IconButton
+                          aria-label="delete"
+                          onClick={() => this.deleteItem(item.id)}
+                          style={{float:'right'}}
+                        >
+                          <DeleteIcon fontSize="small"/>
+                        </IconButton>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </CardContent>
+          </Card>
+        </Container>
+
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/shared/Footer.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      {"Copyright © "}
+      <a
+        color="inherit"
+        target="_blank"
+        href="https://dhirajsonavane.github.io/react-todo/"
+      >
+        React Material UI - To Do App
+      </a>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "10vh",
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: "auto",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[800],
+  },
+}));
+
+export default function Footer() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            Website created by Dhiraj Sonavane (dhiraj.sonavane@gmail.com)
+          </Typography>
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+  );
+}
