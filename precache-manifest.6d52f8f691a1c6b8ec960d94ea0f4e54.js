66503b660c577cf1a848421289f8bd3654876ade
Sonavane, Dhiraj <Dhiraj.Sonavane@tabcorp.com.au>
2020-04-11 16:54:35 +1000

    Updates

---
+++ precache-manifest.6d52f8f691a1c6b8ec960d94ea0f4e54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85a63b4d158038ba945201765e5bacc",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "794347396f09dfd5607f",
+    "url": "/react-todo/static/css/main.8bc11fb3.chunk.css"
+  },
+  {
+    "revision": "1fc8db18a288d913efbb",
+    "url": "/react-todo/static/js/2.4cd5a720.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo/static/js/2.4cd5a720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794347396f09dfd5607f",
+    "url": "/react-todo/static/js/main.b1b1a2a7.chunk.js"
+  },
+  {
+    "revision": "c3f0068e021411733572",
+    "url": "/react-todo/static/js/runtime-main.fd60b479.js"
+  }
+]);
